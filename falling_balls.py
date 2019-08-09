b763b75469d5cff22f4bdddfb3fd753798c9bf20
Busiky <ya.busiky@gmail.com>
2019-08-09 08:04:31 +0300

    Fall!

--- falling_balls.py
+++ falling_balls.py
@@ -6,6 +6,10 @@ pygame.init()
 size = WIDTH, HEIGHT = 400, 300
 screen = pygame.display.set_mode(size)
 screen.fill(pygame.Color('#000000'))
+clock = pygame.time.Clock()
+v = 100
+x, y = pos = (0, 0)
+color = (200, 0, 0)
 
 # create 2 lists for saving properties of balls
 # coords, colors
@@ -19,8 +23,9 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            pos = event.pos
+            x, y = pos = event.pos
             balls_coords.append(pos)
+
             balls_colors.append(
                 (
                     random.randint(0, 255),
@@ -28,10 +33,19 @@ while running:
                     random.randint(0, 255)
                 )
             )
-    for i in range(len(balls_coords)):
-        pygame.draw.circle(screen, pygame.Color(
-            *balls_colors[i]
-        ), balls_coords[i], 10)
+            color = balls_colors[0]
+    pygame.draw.circle(screen, pygame.Color(*color), (x, int(y)), 10)
+    y += v * clock.tick() / 1000
+
+
+    # for i in range(len(balls_coords)):
+    #     pygame.draw.circle(screen, pygame.Color(
+    #         *balls_colors[i]
+    #     ), balls_coords[i], 10)
+    # for i in range(len(balls_coords)):
+    #     x, y = balls_coords[i][0], int(
+    #         balls_coords[i][1] + v * clock.tick() / 1000)
+    #     balls_coords[i] = (x, y)
 
     pygame.display.flip()
 pygame.quit()
