7dac7a92485e5fe6893d50af9acd6c35bdb5410c
Busiky <ya.busiky@gmail.com>
2019-08-09 12:05:50 +0300

    Fall and draw line

--- falling_balls.py
+++ falling_balls.py
@@ -6,26 +6,37 @@ pygame.init()
 size = WIDTH, HEIGHT = 400, 300
 screen = pygame.display.set_mode(size)
 screen.fill(pygame.Color('#000000'))
+v = 100
+clock = pygame.time.Clock()
 
 # create dict for saving properties of balls
-# (coords): (color)
+# (color): [coords]
 balls = {}
+bottom = {}
 
+screen_2 = pygame.Surface(screen.get_size())
 running = True
 while running:
-    screen.fill(pygame.Color('black'))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
+            screen_2.blit(screen, (0, 0))
             pos = event.pos
-            balls[pos] = pygame.Color(
+            balls[
                 (
                     random.randint(0, 255),
                     random.randint(0, 255),
                     random.randint(0, 255)
-                )
-            )
+                )] = [pos[0], pos[1]]
 
+    screen.fill(pygame.Color('black'))
+    for ball in balls:
+        pygame.draw.circle(screen_2, pygame.Color(*ball), (
+            balls[ball][0], balls[ball][1]
+        ), 10)
+        balls[ball][1] += int(v * clock.tick() / 100)
+        screen.blit(screen_2, (0, 0))
     pygame.display.flip()
 pygame.quit()
