5f57621eb74a3253c9b93e280ca131e70916006f
Busiky <ya.busiky@gmail.com>
2019-08-09 12:25:59 +0300

    Bad idea with dict

--- falling_balls.py
+++ falling_balls.py
@@ -30,13 +30,24 @@ while running:
                     random.randint(0, 255),
                     random.randint(0, 255)
                 )] = [pos[0], pos[1]]
-
-    screen.fill(pygame.Color('black'))
+    print(balls)
+    screen_2.fill(pygame.Color('black'))
     for ball in balls:
         pygame.draw.circle(screen_2, pygame.Color(*ball), (
             balls[ball][0], balls[ball][1]
         ), 10)
         balls[ball][1] += int(v * clock.tick() / 100)
+        if balls[ball][1] >= HEIGHT - 5:
+            bottom[ball] = [balls[ball][0], HEIGHT - 5]
+        screen.fill(pygame.Color('black'))
+        screen.blit(screen_2, (0, 0))
+
+    for ball in bottom:
+        pygame.draw.circle(screen_2, pygame.Color(*ball), (
+            bottom[ball][0], bottom[ball][1]
+        ), 10)
+        if balls[ball][1] >= HEIGHT - 5:
+            del balls[ball]
         screen.blit(screen_2, (0, 0))
     pygame.display.flip()
 pygame.quit()
