77e62e54b25d9ffbde98a91f877aaa1e094656e2
Busiky <ya.busiky@gmail.com>
2019-08-10 19:30:15 +0300

    They falls, but slowly

--- git_falling_balls.py
+++ git_falling_balls.py
@@ -1,10 +1,22 @@
 import pygame
+import random
 
 pygame.init()
 
 size = WIDTH, HEIGHT = 400, 300
 screen = pygame.display.set_mode(size)
 screen.fill(pygame.Color('black'))
+
+clock = pygame.time.Clock()
+v = 100
+
+coord = []
+speed = []
+color = []
+
+pos = (0, 0)
+
+screen_2 = pygame.Surface(screen.get_size())
 
 running = True
 while running:
@@ -13,6 +25,18 @@ while running:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = event.pos
+            coord.append([pos[0], pos[1]])
+            color.append('#{:06x}'.format(random.randrange(0, 0xffffff)))
+            speed.append(v)
+    screen_2.fill(pygame.Color('black'))
+    for i in range(len(coord)):
+        pygame.draw.circle(screen_2, pygame.Color(color[i]), (coord[i][0], int(coord[i][1])), 10)
+        coord[i][1] += speed[i] * clock.tick() / 10
+        if coord[i][1] >= HEIGHT - 10:
+            speed[i] = 0
+
+    # screen.fill(pygame.Color('black'))
+    screen.blit(screen_2, (0, 0))
 
     pygame.display.flip()
 pygame.quit()
